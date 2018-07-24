5a885a2dabf49cb2bc34941131ee098987be2e45
Kaviyarasu15 <kaviyarasu501@gmail.com>
2018-07-24 09:58:26 +0530

    Create armstrongnumber.py

---
+++ python/armstrongnumber.py
@@ -0,0 +1,12 @@
+lower = int(input("Enter first number: "))  
+upper = int(input("Enter second number: "))  
+  
+for num in range(lower,upper + 1):  
+   sum = 0  
+   temp = num  
+   while temp > 0:  
+       digit = temp % 10  
+       sum += digit ** 3  
+       temp //= 10  
+       if num == sum:  
+            print(num)  
