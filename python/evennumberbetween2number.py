df61038472e4a814365a510ddb71c933d71ad8a0
Kaviyarasu15 <kaviyarasu501@gmail.com>
2018-07-24 09:49:17 +0530

    Create evennumberbetween2number.py

---
+++ python/evennumberbetween2number.py
@@ -0,0 +1,5 @@
+lower_limit = int(input("Enter the lower number : "))
+upper_limit = int(input("Enter the upper number : "))
+for i in range(lower_limit,upper_limit+1):
+  if(i%2 == 0):
+    print(i)
